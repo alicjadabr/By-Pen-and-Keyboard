fe3af6b367992343e7a62226e4d8430b95b4b827
Alicja Dabrowska <alicja.dab27@gmail.com>
2023-06-29 16:27:44 +0200

    Build the SideBar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { IoReorderThreeOutline } from 'react-icons/io5'
-import { FaRegBell } from 'react-icons/fa'
+import { FaRegBell, FaUsers } from 'react-icons/fa'
 import { BiEditAlt, BiSearch } from 'react-icons/bi'
 import { HiOutlineChevronDown } from 'react-icons/hi'
+import { MdFavorite } from 'react-icons/md'
+import { PiCursorClickDuotone } from 'react-icons/pi'
 
 const HomePage = () => {
   return (
-    <div className='flex flex-col w-full h-screen'>
+    <div className='flex flex-col w-full h-full'>
       
       <header className='h-20 w-full flex flex-row justify-around items-center
        bg-white border-b-[1px] border-gray-300'>
@@ -38,8 +40,8 @@ const HomePage = () => {
 
       </header>
 
-      <section className='grid w-full h-full grid-cols-12 place-items-center'>
-        <main className='col-span-8 border-r border-gray-300 h-full w-full px-24'>
+      <section className='grid grid-cols-12'>
+        <main className='col-span-8 border-r border-gray-300 px-24'>
           
           {/* Search part start */}
           <div className='flex flex-col space-y-4 w-full py-10'>
@@ -73,18 +75,16 @@ const HomePage = () => {
             </div>
             <div className='w-full justify-between flex items-center 
             border-b border-gray-300 pb-8'>
-              <div>Artykuły</div>
-              <div>
-                <button 
-                  className='flex space-x-2 items-center px-4 py-1.5
-                  border border-gray-800 rounded-3xl'
-                >
-                  <div>Obserwowane</div>
-                  <div>
-                    <HiOutlineChevronDown className='text-xl'/>
-                  </div>
-                </button>
-              </div>
+              <span>Artykuły</span>
+              <button 
+                className='flex space-x-2 items-center px-4 py-1.5
+                border border-gray-800 rounded-3xl'
+              >
+                <div>Obserwowane</div>
+                <div>
+                  <HiOutlineChevronDown className='text-xl'/>
+                </div>
+              </button>
             </div>
           </div>
           {/* Search part end */}
@@ -99,7 +99,7 @@ const HomePage = () => {
                 {/* Article header part (author + date) */}  
                 <div className='flex w-full items-center space-x-3'>
                   {/* Article author image */}
-                  <div className='h-10 w-10 bg-gray-400 rounded-full'></div>
+                  <div className='h-10 w-10 bg-gray-400 rounded-full flex-none'></div>
                   {/* Article author info and articte created date*/}
                   <div>
                     <p className='font-semibold'>Name author &#x2022; 22 mar 2022</p>
@@ -150,7 +150,67 @@ const HomePage = () => {
           </div>
           {/* Articles part end */}
         </main>
-        <aside className='col-span-4 w-full'>sidebar</aside>
+        <aside 
+          className='col-span-4 flex flex-col p-6 space-y-4'
+        >
+          <div>
+            <div className='flex space-x-2 items-center mb-6'>
+              <FaUsers className='text-2xl text-blue-500'/> 
+              <h3 className='font-bold text-lg'>Osoby, które mogą Cię zainteresować</h3>
+            </div>
+            <div className='flex flex-col space-y-8'>
+              {Array.from({ length: 4 }).map((_,i) => (
+                <div 
+                  key={i}
+                  className='flex space-x-5 items-center'
+                >
+                  <div className='bg-gray-300 w-10 h-10 rounded-full flex-none'></div>
+                  <div>
+                    <div className='text-gray-900 font-bold text-sm'>Jan Kowalski</div>
+                    <div className='text-xs'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi veniam officiis modi culpa.</div>
+                  </div>
+               
+                  <button 
+                    className='transition ease-in-out flex space-x-2 items-center px-3 py-1.5
+                    border border-gray-400/50 rounded-3xl hover:scale-110
+                     hover:bg-indigo-200 duration-300'
+                  >
+                    <div className='text-sm'>Obserwuj</div>
+                    <div>
+                      <PiCursorClickDuotone className='text-md'/>
+                    </div>
+                  </button>
+                    
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className='sticky top-20'>
+            <div className='flex space-x-2 items-center mt-3 mb-5'>
+              <MdFavorite className='text-2xl text-red-400'/> 
+              <h3 className='font-bold text-lg'>Zapisane posty</h3>
+            </div>
+            <div className='flex flex-col space-y-8'>
+              {Array.from({ length: 5}).map((_,i) => (
+                <div key={i} className='flex space-x-6 items-center group'>
+                  <div className='h-full w-2/5 rounded-xl bg-gray-300 aspect-square'></div>
+                  <div className='w-3/5 flex flex-col space-y-2'>
+                    <div className='text-lg font-semibold transition ease-in-out duration-200
+                    group-hover:-translate-y-2 hover:text-indigo-400'>
+                      Lorem ipsum dolor sit amet consectetur.
+                    </div>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam maxime modi amet voluptates vel.</div>
+                    <div className='flex space-x-2 items-center w-full'>
+                      <div className='w-8 h-8 bg-gray-300 rounded-full flex-none'></div>
+                      <div>Jan Kowalski  &#x2022;</div>
+                      <div>22 mar 2022</div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </aside>
       </section>
 
     </div>
