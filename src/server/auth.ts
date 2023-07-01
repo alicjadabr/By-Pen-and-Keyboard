cc577264e27ac202c50c938e179e076e96d7c723
Alicja Dabrowska <alicja.dab27@gmail.com>
2023-07-01 15:11:26 +0200

    Save username on sign up using NextAuth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,13 +1,14 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { type GetServerSidePropsContext } from "next";
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { type GetServerSidePropsContext } from 'next'
 import {
   getServerSession,
   type NextAuthOptions,
   type DefaultSession,
-} from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { env } from "~/env.mjs";
-import { prisma } from "~/server/db";
+} from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { env } from '~/env.mjs'
+import { prisma } from '~/server/db'
+import { generateUsername } from '~/utils/generateUsername'
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -15,18 +16,18 @@ import { prisma } from "~/server/db";
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session extends DefaultSession {
     user: {
-      id: string;
+      id: string
       // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
+      // role: UserRole
+    } & DefaultSession['user']
   }
 
   // interface User {
   //   // ...other properties
-  //   // role: UserRole;
+  //   // role: UserRole
   // }
 }
 
@@ -50,18 +51,19 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          username: generateUsername(profile.name)
+        }
+      },
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+
   ],
-};
+}
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
@@ -69,8 +71,8 @@ export const authOptions: NextAuthOptions = {
  * @see https://next-auth.js.org/configuration/nextjs
  */
 export const getServerAuthSession = (ctx: {
-  req: GetServerSidePropsContext["req"];
-  res: GetServerSidePropsContext["res"];
+  req: GetServerSidePropsContext['req']
+  res: GetServerSidePropsContext['res']
 }) => {
-  return getServerSession(ctx.req, ctx.res, authOptions);
-};
+  return getServerSession(ctx.req, ctx.res, authOptions)
+}
